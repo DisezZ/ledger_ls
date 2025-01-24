995a085e88d81b80ad61cb10a6ceb1891bebb00a
DisezZ <lutfeedeemae@gmail.com>
2025-01-24 07:54:08 +0700

    feat: show account comp on trigger char

--- src/ledger.rs
+++ src/ledger.rs
@@ -1,19 +1,54 @@
 use std::collections::HashSet;
 
 use tower_lsp::lsp_types::CompletionParams;
+use tree_sitter::Node;
 
+#[derive(Default)]
 pub struct Ledger {
-    pub accounts: HashSet<String>,
+    accounts: HashSet<String>,
     pub payees: HashSet<String>,
 }
 
 impl Ledger {
     pub fn new() -> Self {
-        let mut accounts = HashSet::new();
-        Self {
-            accounts,
-            payees: HashSet::new(),
-        }
+        Default::default()
+    }
+
+    pub fn process_text(&mut self, s: &String) {
+        let mut parser = tree_sitter::Parser::new();
+        parser.set_language(tree_sitter_ledger::language());
+        let tree = parser.parse(&s, None).unwrap();
+        traverse(tree.root_node(), &mut |node: Node| {
+            println!(
+                "Node: {}, Text: {:?}",
+                node.kind(),
+                // &s[node.start_byte()..node.end_byte()],
+                s.get(node.start_byte()..node.end_byte())
+            );
+            if node.kind() == "account" {
+                self.accounts
+                    .insert(s[node.start_byte()..node.end_byte()].into());
+            } else if node.kind() == "payee" {
+                self.payees
+                    .insert(s[node.start_byte()..node.end_byte()].into());
+            }
+        });
+    }
+
+    pub fn get_accounts(&self) -> Vec<String> {
+        self.accounts.clone().into_iter().collect()
+    }
+
+    pub fn get_payees(&self) -> Vec<String> {
+        self.payees.clone().into_iter().collect()
+    }
+}
+
+fn traverse(node: Node, f: &mut impl FnMut(Node)) {
+    f(node);
+    let mut cursor = node.walk();
+    for child in node.children(&mut cursor) {
+        traverse(child, f);
     }
 }
 
@@ -26,42 +61,67 @@ mod test {
     use crate::ledger::Ledger;
 
     #[test]
-    fn create_ledger() {
-        let s = "2025-01-01 Test Payerr\n\tExpenses:Dinner\t$12.00\n\tAssets:Wallet\n";
-        let mut parser = tree_sitter::Parser::new();
-        parser.set_language(tree_sitter_ledger::language());
-        let tree = parser.parse(s, None).unwrap();
+    fn get_all_accounts() {
+        // arrange
+        let s = "2025-01-01 Test Payerr\n\tExpenses:Dinner\t$12.00\n\tAssets:Wallet\n".to_string();
         let mut ledger = Ledger::new();
-        let mut cursor = tree.walk();
-        cursor.goto_first_child();
-        cursor.goto_next_sibling();
-        // ledger.accounts.insert(cursor.node().kind().into());
-        traverse(tree.root_node(), &mut |node: Node| {
-            println!(
-                "Node: {}, Text: {:?}",
-                node.kind(),
-                &s[node.start_byte()..node.end_byte()]
-            );
-            if node.kind() == "account" {
-                ledger
-                    .accounts
-                    .insert(s[node.start_byte()..node.end_byte()].into());
-            }
-        });
 
+        // act
+        ledger.process_text(&s);
+        let a: HashSet<String> = HashSet::from_iter(ledger.get_accounts());
+
+        // assert
         assert_eq!(
-            ledger.accounts,
-            HashSet::from_iter::<Vec<String>>(
-                vec!["Expenses:Dinner".into(), "Assets:Wallet".into()].into()
-            )
+            a,
+            HashSet::from_iter::<Vec<String>>(vec![
+                "Expenses:Dinner".to_string(),
+                "Assets:Wallet".to_string()
+            ])
         );
     }
 
-    fn traverse(node: Node, f: &mut impl FnMut(Node)) {
-        f(node);
-        let mut cursor = node.walk();
-        for child in node.children(&mut cursor) {
-            traverse(child, f);
-        }
+    #[test]
+    fn get_all_payees() {
+        // arrange
+        let s = "2025-01-01 Test Payerr\n\tExpenses:Dinner\t$12.00\n\tAssets:Wallet\n".to_string();
+        let mut ledger = Ledger::new();
+
+        // act
+        ledger.process_text(&s);
+
+        // assert
+        assert_eq!(ledger.get_payees(), vec!["Test Payerr".to_string()]);
     }
+
+    // #[test]
+    // fn create_ledger() {
+    //     let s = "2025-01-01 Test Payerr\n\tExpenses:Dinner\t$12.00\n\tAssets:Wallet\n";
+    //     let mut parser = tree_sitter::Parser::new();
+    //     parser.set_language(tree_sitter_ledger::language());
+    //     let tree = parser.parse(s, None).unwrap();
+    //     let mut ledger = Ledger::new();
+    //     let mut cursor = tree.walk();
+    //     cursor.goto_first_child();
+    //     cursor.goto_next_sibling();
+    //     // ledger.accounts.insert(cursor.node().kind().into());
+    //     traverse(tree.root_node(), &mut |node: Node| {
+    //         println!(
+    //             "Node: {}, Text: {:?}",
+    //             node.kind(),
+    //             &s[node.start_byte()..node.end_byte()]
+    //         );
+    //         if node.kind() == "account" {
+    //             ledger
+    //                 .accounts
+    //                 .insert(s[node.start_byte()..node.end_byte()].into());
+    //         }
+    //     });
+    //
+    //     assert_eq!(
+    //         ledger.accounts,
+    //         HashSet::from_iter::<Vec<String>>(
+    //             vec!["Expenses:Dinner".into(), "Assets:Wallet".into()].into()
+    //         )
+    //     );
+    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ use trace::setup_logging;
 
 #[tokio::main]
 async fn main() {
-    setup_logging();
-    tesssitter_parser::parse();
+    let _guard = setup_logging();
+    // tesssitter_parser::parse();
 
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
@@ -18,6 +18,7 @@ async fn main() {
 
 mod ls {
     use std::collections::HashSet;
+    use std::sync::{Arc, RwLock};
 
     use tower_lsp::jsonrpc::Result;
     use tower_lsp::{lsp_types::*, LspService, Server};
@@ -30,7 +31,7 @@ mod ls {
     pub struct Backend {
         pub client: Client,
         pub language: Language,
-        pub ledger: Ledger,
+        pub ledger: Arc<RwLock<Ledger>>,
     }
 
     impl Backend {
@@ -40,19 +41,30 @@ mod ls {
             Self {
                 client,
                 language,
-                ledger,
+                ledger: Arc::new(RwLock::new(ledger)),
             }
+        }
+
+        fn account_completion() -> Vec<CompletionItem> {
+            vec![]
+        }
+
+        fn payee_completion() -> Vec<CompletionItem> {
+            vec![]
         }
     }
 
     #[tower_lsp::async_trait]
     impl LanguageServer for Backend {
         async fn initialize(&self, params: InitializeParams) -> Result<InitializeResult> {
-            _ = self.ledger.accounts;
+            debug!("Initialize");
             Ok(InitializeResult {
                 capabilities: ServerCapabilities {
+                    text_document_sync: Some(TextDocumentSyncCapability::Kind(
+                        TextDocumentSyncKind::FULL,
+                    )),
                     completion_provider: Some(CompletionOptions {
-                        trigger_characters: Some(vec![".".into()]),
+                        trigger_characters: Some(vec![":".into(), ".".into()]),
                         ..Default::default()
                     }),
                     ..Default::default()
@@ -65,30 +77,34 @@ mod ls {
             self.client
                 .log_message(MessageType::INFO, "server initialized!")
                 .await;
-            debug!("server initialized!");
+        }
+
+        async fn did_open(&self, params: DidOpenTextDocumentParams) {
+            debug!("Document open");
+        }
+
+        async fn did_change(&self, params: DidChangeTextDocumentParams) {
+            let mut ledger = self.ledger.write().unwrap();
+            ledger.process_text(&params.content_changes[0].text);
+        }
+
+        async fn did_close(&self, params: DidCloseTextDocumentParams) {
+            debug!("Document close");
         }
 
         async fn completion(&self, params: CompletionParams) -> Result<Option<CompletionResponse>> {
             if let Some(ctx) = params.context {
                 if let Some(trigger_char) = ctx.trigger_character {
                     match trigger_char.as_str() {
-                        "." => {
-                            debug!("completion list");
+                        ":" | "." | _ => {
                             let v = vec![1];
-                            let mut items = self
-                                .ledger
-                                .accounts
+                            let mut ledger = self.ledger.write().unwrap();
+                            let mut items = ledger
+                                .get_accounts()
                                 .iter()
-                                .map(|e| CompletionItem::new_simple("Q".into(), "Q1".into()))
+                                .map(|e| CompletionItem::new_simple(e.clone(), "Account".into()))
                                 .collect::<Vec<CompletionItem>>();
-                            items.extend::<Vec<CompletionItem>>(
-                                vec![CompletionItem::new_simple(
-                                    "test completion label".into(),
-                                    "test completion detail".into(),
-                                )]
-                                .into(),
-                            );
-                            // items.extend();
+                            let comp = ledger.get_accounts();
                             return Ok(Some(CompletionResponse::List(CompletionList {
                                 is_incomplete: false,
                                 items: items,
@@ -110,9 +126,9 @@ mod ls {
 mod trace {
     use std::fs::OpenOptions;
 
-    use tracing::level_filters::LevelFilter;
+    use tracing::{debug, level_filters::LevelFilter};
 
-    pub fn setup_logging() {
+    pub fn setup_logging() -> tracing_appender::non_blocking::WorkerGuard {
         let mut file = OpenOptions::new()
             .append(true)
             .open("/home/lutfee/dev/projects/ledger_ls/logs/server.log")
@@ -129,6 +145,7 @@ mod trace {
             .with_line_number(true)
             .finish();
         tracing::subscriber::set_global_default(subscriber).unwrap();
+        _guard
     }
 }
 
@@ -167,15 +184,13 @@ mod tesssitter_parser {
             .open("./tree.graph.dot")
             .unwrap();
         tree.print_dot_graph(&file);
-        println!("{:?}", tree);
-        // let mut cursor = tree.walk();
-        // tree.root_node()
-        //     .children(&mut cursor)
-        //     .for_each(|e| println!("{:?}", e));
-        traverse_child(tree.walk());
     }
+}
 
-    fn traverse_child(cursor: TreeCursor) {
-        // cursor.clone_into
+#[cfg(test)]
+mod test {
+    #[test]
+    fn show_account_completion_when_appropriate() {
+        todo!();
     }
 }
